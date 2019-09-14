3774e0825e1d0944a02793c59472f8d7a0e39fb4
Kels <dojo565@gmail.com>
2019-09-14 13:51:33 -0400

    first commit

---
+++ IsLucky/IsLucky/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsLucky
+{
+    class Class1
+    {
+        public bool IsLucky(int ticketNum)
+        {
+            int[] outarry = Array.ConvertAll(ticketNum.ToString().ToArray(), x => (int)x - 48);
+
+
+
+            Console.WriteLine(outarry[0]);
+            Console.ReadKey();
+
+            return true;
+        }
+    }
+}
---
+++ IsLucky/IsLucky/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsLucky
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Console.ReadKey();
+
+            Class1 test = new Class1();
+
+            test.IsLucky(1234);
+        }
+    }
+}
